eb9e6d264e182136319284c12cab15a585646fb0
TangSY <357770@whut.edu.cn>
2026-07-20 20:31:26 +0800

    chore(build): 初始化 Maven 多模块父工程

---
+++ src/main/java/com/tsy/osbackend/OsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tsy.osbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsBackendApplication.class, args);
+	}
+
+}
